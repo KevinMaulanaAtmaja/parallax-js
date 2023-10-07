b52fbfead429d7e6644fa2a77d19397a081b22ef
Kevin Maulana Atmaja <j04719671@gmail.com>
2023-10-07 20:23:13 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(window).scroll(function() {
+    var wScroll = $(this).scrollTop();
+     
+    $('h1').css({
+        'transform' : 'translate(0px, '+ wScroll/2 +'%)'
+    })
+
+    $('.kotak').css({
+        'transform' : 'translate(0px, '+ wScroll/10 +'%)'
+    })
+    // console.log(wScroll);
+});
